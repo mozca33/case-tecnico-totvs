149a9e013b1c5b2dfae7dba58eb7cec9219c6c5d
Rafael Felipe <rafaelfelipe501@gmail.com>
2025-05-26 21:59:01 -0300

    feat: implement ClientException and GlobalExceptionHandler for improved error handling

--- backend/src/main/java/com/rafael/clients/domain/exception/ClientException.java
+++ backend/src/main/java/com/rafael/clients/domain/exception/ClientException.java
@@ -1,5 +1,8 @@
 package com.rafael.clients.domain.exception;
 
-public class ClientException {
+public class ClientException extends RuntimeException {
+    public ClientException(String message) {
+        super(message);
+    }
 
 }
--- backend/src/main/java/com/rafael/clients/interfaces/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/rafael/clients/interfaces/handler/GlobalExceptionHandler.java
@@ -1,5 +1,16 @@
 package com.rafael.clients.interfaces.handler;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.rafael.clients.domain.exception.ClientException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ClientException.class)
+    public ResponseEntity<String> handleClientException(ClientException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
 }
