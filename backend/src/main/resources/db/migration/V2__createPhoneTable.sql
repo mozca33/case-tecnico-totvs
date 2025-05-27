553c6fbf6ba328b287a3ea49f5be3d88add19495
Rafael Felipe <rafaelfelipe501@gmail.com>
2025-05-26 21:57:51 -0300

    feat: add SQL migration scripts for clients, phones, and addresses tables

---
+++ backend/src/main/resources/db/migration/V2__createPhoneTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE phones (
+    id UUID PRIMARY KEY,
+    client_id UUID NOT NULL,
+    number VARCHAR(20) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_client_phone FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE
+);
---
+++ backend/src/main/resources/db/migration/V3__createAddressTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE addresses (
+    id UUID PRIMARY KEY,
+    client_id UUID NOT NULL,
+    street VARCHAR(100) NOT NULL,
+    number VARCHAR(10),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    zip_code VARCHAR(10),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_client_address FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE
+);
